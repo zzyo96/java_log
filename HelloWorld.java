86d7d00fa7946b05732272b8ae7610d474a8acd6
zzyo <1977795940@qq.com>
2019-11-28 14:31:29 +0800

    写一个hello world

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.print("你好 阿月");
+	}
+}
+
+class Hello{
+	public static void main(String[] args){
+		System.out.print("你好");
+	}
+}
