069e77c6ff3a7759cd3fd503fb8b5395fba8f7e9
zzyo <1977795940@qq.com>
2019-11-29 22:56:05 +0800

    循环中continue使用

---
+++ for01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class for01{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+	int count = 0;
+	for(int i = 0;i<=5;i++){
+	double score = input.nextDouble();
+	if(score < 90){
+	continue; //结束本次循环，继续执行下一次循环
+}
+}
+}
+}
